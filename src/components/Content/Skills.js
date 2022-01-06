e5039237a9493a1d38f2e4a62783fd02c1d2139e
Datrinon <dtrinh089@gmail.com>
2022-01-06 13:10:49 -0800

    Add HTML for skills section

--- src/components/Content/Skills.js
+++ src/components/Content/Skills.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import Section from './Section'
 
+import { SKILLS } from '../../data/skills'
+
 function Skills() {
-  
+
   return (
     <Section title={"My Skills"}>
-      Skills here.
+      {
+        SKILLS.map((list, index) => {
+          return (
+            <div key={index}>
+              <h3>{list.category}</h3>
+              <ul>
+                {list.skills.map((skill, index) => {
+                  return (
+                    <li key={index}>
+                      <skill.icon/>
+                      <span>{skill.name}</span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })
+      }
     </Section>
   )
 }
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,13 +1,16 @@
 import { FaHtml5, FaReact } from "react-icons/fa"
-import { DiCss3 } from "react-icons/di"
+import { DiCss3, DiJava, DiPython, DiGit } from "react-icons/di"
 import { IoLogoJavascript } from "react-icons/io"
+import { SiWebpack, SiFirebase } from "react-icons/si"
+import { VscTerminalBash } from "react-icons/vsc"
+import { AiFillFileExcel } from "react-icons/ai"
 
 export const SKILLS = [
   {
-    category: "Front-End",
+    category: "Front-End / Web",
     skills: [
       {
-        name: "React",
+        name: "React.js",
         icon: FaReact
       },
       {
@@ -19,9 +22,47 @@ export const SKILLS = [
         icon: DiCss3
       },
       {
-        name: "JS",
+        name: "JavaScript",
         icon: IoLogoJavascript
+      },
+      {
+        name: "Firebase",
+        icon: SiFirebase
       }
     ]
   },
+  {
+    category: "General Purpose Languages",
+    skills: [
+      {
+        name: "Java",
+        icon: DiJava
+      },
+      {
+        name: "Python",
+        icon: DiPython
+      }
+    ]
+  },
+  {
+    category: "Other Technologies",
+    skills: [
+      {
+        name: "BASH / Unix",
+        icon: VscTerminalBash
+      },
+      {
+        name: "Git",
+        icon: DiGit
+      },
+      {
+        name: "Webpack",
+        icon: SiWebpack
+      },
+      {
+        name: "Excel",
+        icon: AiFillFileExcel
+      }
+    ]
+  }
 ]
