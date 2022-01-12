13359a09c64e5c72be7deab1bcc204396a78816d
Datrinon <dtrinh089@gmail.com>
2022-01-12 11:22:22 -0800

    Remove unused CSS from about component

--- src/components/Content/styled/About.styled.js
+++ src/components/Content/styled/About.styled.js
@@ -10,26 +10,6 @@ export const AboutContainer = styled.div`
   & > * {
     margin: 0.5em auto;
   }
-
-  & .section-contents {
-    /* display: grid;
-    grid-template-areas:
-    "pic"
-    "bio";
-    grid-template-rows: auto 1fr;
-
-    & .bio {
-      grid-area: bio;
-    }
-
-    & .profile-pic-container {
-      grid-area: pic;
-    } */
-
-    @media ${device.tablet} {
-      all: unset;
-    }
-  }
 `
 
 export const AboutHeader = styled.header`
@@ -45,44 +25,6 @@ export const AboutHeading = styled.h2`
   padding: 1em 0;
 `
 
-export const AboutMe = styled.ul`
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100%;
-
-  & li {
-    position: absolute;
-  }
-
-  & .fact-1 {
-    top: 0em;
-    right: 105%;
-    width: fit-content;
-  }
-
-  & .fact-2 {
-    top: 15%;
-    left: 110%;
-  }
-
-  & .fact-3 {
-    top: 40%;
-    right: 110%;
-  }
-
-  & .fact-4 {
-    top: 75%;
-    left: 110%;
-  }
-
-  @media ${device.mobileL} {
-    & li {
-      width: fit-content;
-    }
-  }
-`
-
 export const AboutMeFacts = styled.ul`
   li:not(:last-child)::after  {
     content: ", ";
@@ -95,31 +37,13 @@ export const AboutMeFacts = styled.ul`
 `
 
 export const AboutMeFact = styled.li`
-    display: inline;
-    animation: ${appear} ${props => props.ms ? `${props.ms}ms` : "300ms"};
+  display: inline;
+  animation: ${appear} ${props => props.ms ? `${props.ms}ms` : "300ms"};
 
-    @media ${device.mobileL} {
-      width: fit-content;
-    }
-
-    &::after {
-    }
-`
-
-export const ProfilePic = styled.div`
-  position: relative;
-  width: 128px;
-  height: 128px;
-
-  & .profile-pic {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 50%;
+  @media ${device.mobileL} {
+    width: fit-content;
   }
 
-  @media ${device.tablet} {
-    width: 200px;
-    height: 200px;
+  &::after {
   }
 `
