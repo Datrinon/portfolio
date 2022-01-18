96a6989e735f2c3c815814305dfe7800a0931b5b
Datrinon <dtrinh089@gmail.com>
2022-01-18 15:11:31 -0800

    Implement transparency behind sidebar menu
    Here's the trick:
    - Make it a direct descendant of the header rather than its menu element, which allows you to set the stacking context of it to be below the menu element
    - Set the offset to 100% so it appears below the top bar.

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -25,6 +25,17 @@ export const Header = styled.header`
     transition: transform 300ms;
   }
 
+  &::after {
+      content: "";
+      transition: background-color 300ms;
+      background-color: ${props => props.menuOpen ? "#0000008c" : "transparent"}; 
+      width: 100vw;
+      height: 100vh;
+      position: absolute;
+      z-index: -1;
+      top: 100%;
+    }
+
   & .logo {
     align-self: center;
   }
