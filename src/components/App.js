4854d75e1fa2e81c9bd740af657619eaf5a6b508
Datrinon <dtrinh089@gmail.com>
2022-01-19 02:39:47 -0800

    Add localStorage support for last used website theme

--- src/components/App.js
+++ src/components/App.js
@@ -18,27 +18,29 @@ import triangle from "../data/decals/triangle.svg";
 import polygon from "../data/decals/polygon.svg";
 import wave from "../data/decals/wave.svg";
 
-import {BsArrowBarUp} from "react-icons/bs";
+import { BsArrowBarUp } from "react-icons/bs";
 import ContactButtons from './Content/ContactButtons';
 import { LIGHT_THEME, DARK_THEME } from './_styled/colors.styled';
 
 export const DarkModeContext = React.createContext(null);
 export const ThemeContext = React.createContext(null);
 
+const prevTheme = window.localStorage.getItem("darkMode");
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(prevTheme === "1");
   const [theme, setTheme] = useState(LIGHT_THEME);
 
-  const [ contactsVisible, setContactsVisible ] = useState(true);
-  const [ topAnchorVisible, setTopAnchorVisible ] = useState(false);
+  const [contactsVisible, setContactsVisible] = useState(true);
+  const [topAnchorVisible, setTopAnchorVisible] = useState(false);
   // IO code here.
   const footerRef = useRef(null);
   const landingRef = useRef(null);
   const topAnchorIntersectionObserver = useRef(null);
   const contactsIntersectionObserver = useRef(null);
 
+  // this use effect sets up some intersection observers for decal purposes.
   useEffect(() => {
     // set up intersection observers to watch if we get to element of "about me"
     // then show the arrow bar up thing.
@@ -75,10 +77,10 @@ function App() {
     };
 
     topAnchorIntersectionObserver.current =
-        new IntersectionObserver(onLandingIntersect, observerOptions);
+      new IntersectionObserver(onLandingIntersect, observerOptions);
 
     contactsIntersectionObserver.current =
-        new IntersectionObserver(onFooterIntersect, observerOptions);
+      new IntersectionObserver(onFooterIntersect, observerOptions);
 
     topAnchorIntersectionObserver.current.observe(landingRef.current);
     contactsIntersectionObserver.current.observe(footerRef.current);
@@ -92,25 +94,15 @@ function App() {
   useEffect(() => {
     if (darkMode) {
       setTheme(DARK_THEME)
+      window.localStorage.setItem("darkMode", "1");
     } else {
       setTheme(LIGHT_THEME)
+      window.localStorage.setItem("darkMode", "0");
     }
   }, [darkMode]);
 
   return (
     <DarkModeContext.Provider value={darkMode}>
-      
-      <A.Page className="page-container" darkMode={darkMode}>
-        <Header setDarkMode={setDarkMode}/>
-        <A.Main role="main" theme={theme}>
-          <div id="landing" className="landing" ref={landingRef}>
-            <Landing />
-            {/* <A.FooterTriangle src={triangle}/> */}
-          </div>
-          <div id="about" className="about page-section">
-            <A.DividingShape
-              src={triangle}
-              $bot={"3px"}
       <ThemeContext.Provider value={theme}>
 
         <A.Page className="page-container" darkMode={darkMode}>
@@ -125,10 +117,6 @@ function App() {
                 src={triangle}
                 $bot={"3px"}
               />
-            <About />
-          </div>
-          <div id="skills" className="skills page-section">
-            {/* <A.DividingShape
               <About />
             </div>
             <div id="skills" className="skills page-section">
@@ -136,31 +124,20 @@ function App() {
               src={triangle}
               $rot={"rotateY(180deg)"}
               /> */}
-            {/* <A.DividingShape
               {/* <A.DividingShape
               src={polygon}
               $rot={"rotateY(180deg)"}
               $bot={"1px"}
               /> */}
-            <Skills />
-            {/* <A.DividingShape
               <Skills />
               {/* <A.DividingShape
               src={polygon}
               $rot={"rotateX(180deg)"}
               /> */}
-            {/* <A.DividingShape
               {/* <A.DividingShape
               src={triangle}
               $rot={"rotate(180deg)"}
               /> */}
-          </div>
-          <div id="projects" className="projects page-section">
-          <A.DividingShape
-              className="heading-divider"
-              src={wave}
-              $rot={"rotate(180deg)"}
-              $bot={"5px"}
             </div>
             <div id="projects" className="projects page-section">
               <A.DividingShape
@@ -169,38 +146,12 @@ function App() {
                 $rot={"rotate(180deg)"}
                 $bot={"5px"}
               />
-            <Projects />
-            <A.DividingShape
-              className="footer-divider"
-              src={wave}
-              $top={"3px"}
               <Projects />
               <A.DividingShape
                 className="footer-divider"
                 src={wave}
                 $top={"3px"}
               />
-          </div>
-          <div id="resume" className="resume page-section">
-
-            <Resume />
-          </div>
-        </A.Main>
-        <footer id="contact" className="page-section" ref={footerRef}>
-          <Contact />
-        </footer>
-        <A.FloatingContactButtonGroup
-          className="floating-contact-buttons"
-          $display={contactsVisible}>
-          <ContactButtons />
-        </A.FloatingContactButtonGroup>
-        <A.FloatingReturnButton
-          href="#"
-          onClick={() => console.log("Clicked anchor")}
-          $display={topAnchorVisible}>
-          <BsArrowBarUp/>
-        </A.FloatingReturnButton>
-      </A.Page>
             </div>
             <div id="resume" className="resume page-section">
 
